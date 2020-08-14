d8dc64c5ae87612f9829e2e9ba8d9d9f4e083842
Ludowik <>
2020-08-14 11:08:24 +0200

    no message

---
+++ graphics/colors.lua
@@ -0,0 +1,16 @@
+black = Color(0)
+white = Color(1)
+
+gray = Color(0.5)
+
+red   = Color(1, 0, 0)
+green = Color(0, 1, 0)
+blue  = Color(0, 0, 1)
+
+yellow  = Color(1, 1, 0)
+magenta = Color(1, 0, 1)
+cyan    = Color(0, 1, 1)
+
+brown = Color(165, 42, 42, 255)
+
+transparent = Color(0, 0, 0, 0)
